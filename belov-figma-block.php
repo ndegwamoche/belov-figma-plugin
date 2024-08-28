e21b8965b1a550bca5263a697b08f7bf28862584
ndegwamoche <151727514+ndegwamoche@users.noreply.github.com>
2024-08-28 20:10:54 +0300

    Initial setup

---
+++ belov-figma-block.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Plugin Name: Belov Figma Block Plugin
+ * Description: This project is a conversion of a pixel-perfect HTML version of a Figma design into a fully functional WordPress ACF block. 
+ * Requires at least: 5.0
+ * Requires PHP: 7.0
+ * Version: 1.0.0
+ * Author: Martin Ndegwa Moche
+ * Author URI: https://github.com/ndegwamoche/
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: belov-figma-block
+ * 
+ */
