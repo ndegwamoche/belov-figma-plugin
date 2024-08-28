ca576204a46fcdd74fb3306682043f4f715f3cb7
ndegwamoche <151727514+ndegwamoche@users.noreply.github.com>
2024-08-28 22:22:07 +0300

    feat: Add fonts and wide feature

--- belov-figma-block.php
+++ belov-figma-block.php
@@ -18,10 +18,17 @@ if (! defined('ABSPATH')) {
     exit;
 }
 
-function belov_figma_block()
-{
+function belov_figma_block() {
     // Register the block.
     register_block_type(__DIR__ . '/blocks/belov-figma');
 }
 
 add_action('init', 'belov_figma_block');
+
+function belov_figma_block_styles_and_scripts() {
+    wp_register_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,100..900;1,100..900&family=Lora:ital,wght@0,400..700;1,400..700&display=swap', array(), null);
+    wp_enqueue_style('google-fonts');
+}
+
+add_action('wp_enqueue_scripts', 'belov_figma_block_styles_and_scripts');
+add_action('admin_enqueue_scripts', 'belov_figma_block_styles_and_scripts');
